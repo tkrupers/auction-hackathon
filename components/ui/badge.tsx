3bffdb2de5c2fa1b5b933ddb36cc57a93d404b45
AndreasSofos <andreas@pxr.nl>
2024-11-22 14:36:33 +0100

    Change badge tetx color on light mode

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -13,7 +13,7 @@ const badgeVariants = cva(
                 destructive:
                     'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
                 outline: 'text-foreground',
-                active: 'border-transparent bg-active text-success-foreground hover:bg-success/80',
+                active: 'border-transparent bg-active text-active-foreground hover:bg-active/80',
             },
         },
         defaultVariants: {
